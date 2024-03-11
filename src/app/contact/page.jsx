62fc546f75a7cfd180a35dd75a73cfd94943d23b
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2024-03-11 10:36:25 +0300

    last

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import style from './contact.module.css';
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.imgContainer}></div>
+    </div>
+  );
 };
 
 export default ContactPage;
